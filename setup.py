3755c0f339c6f07d59241bf968f5bb5d8fa549b7
Jamie Harris <jamiedodgerification12345@gmail.com>
2021-03-23 16:23:43 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='setup_py_test',
+    version='1.0.0',
+    packages=['setup_test'],
+    url='',
+    license='MIT',
+    author='Jamie',
+    author_email='fake@fake.com',
+    description='Setup.py test'
+)
